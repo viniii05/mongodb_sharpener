37b49abb3efcc11091047c52c14aae81259bfff0
viniii05 <vinivt.0520@gmail.com>
2025-03-19 20:45:20 +0530

    Unknown route

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Middleware to parse JSON requests (for POST requests)
 app.use(express.json());
 
 // Routes
-app.get("/orders", (req, res) => {
-    res.send("Here is the list of all orders.");
+app.get("/products", (req, res) => {
+    res.send("Here is the list of all products.");
 });
 
-app.post("/orders", (req, res) => {
-    res.send("A new order has been created.");
+app.post("/products", (req, res) => {
+    res.send("A new product has been added.");
 });
 
-app.get("/users", (req, res) => {
-    res.send("Here is the list of all users.");
+app.get("/categories", (req, res) => {
+    res.send("Here is the list of all categories.");
 });
 
-app.post("/users", (req, res) => {
-    res.send("A new user has been added.");
+app.post("/categories", (req, res) => {
+    res.send("A new category has been created.");
+});
+
+// Wildcard Route - Handle Undefined Routes (404)
+app.use("*", (req, res) => {
+    res.status(404).send("<h1>404 - Page Not Found</h1>");
 });
 
 // Start the server
