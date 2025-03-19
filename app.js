0cadb8529069b5ae7b5652992bc7540ca7fe6e01
viniii05 <vinivt.0520@gmail.com>
2025-03-19 20:41:14 +0530

    completed routes

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-const express = require('express');
-
-const adminRoutes = require('./routes/admin');
-const mongoConnect = require('./util/database').mongoConnect;
+const express = require("express");
 const app = express();
+const port = 3000;
 
-app.use(adminRoutes);
-mongoConnect(() => {
-    app.listen(3000);
-})
+// Middleware to parse JSON requests (for POST requests)
+app.use(express.json());
+
+// Routes
+app.get("/orders", (req, res) => {
+    res.send("Here is the list of all orders.");
+});
+
+app.post("/orders", (req, res) => {
+    res.send("A new order has been created.");
+});
+
+app.get("/users", (req, res) => {
+    res.send("Here is the list of all users.");
+});
+
+app.post("/users", (req, res) => {
+    res.send("A new user has been added.");
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
